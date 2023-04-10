49f589bc077a9e5eff944f11de4d4ab43adead50
nikitabarnawal@gmail.com <nikitabarnawal@gmail.com>
2023-04-10 20:53:59 +0530

    added valid error message

--- src/errorPage.tsx
+++ src/errorPage.tsx
@@ -5,8 +5,7 @@ export default function ErrorPage() {
 
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <h1>Oops! This page is under Maintenance</h1>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
